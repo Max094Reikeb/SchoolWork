37237d3cd317982d159e7f74c47f4b3cb5dd6e60
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2024-02-28 16:52:47 +0100

    Lot of changes to the app

--- mobile-development/android/ViewActivity/app/src/main/java/com/example/viewactivity/MainActivity.kt
+++ mobile-development/android/ViewActivity/app/src/main/java/com/example/viewactivity/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.example.viewactivity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.StyleSpan
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        supportActionBar?.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.gradient))
+        setContentView(R.layout.detail)
+        supportActionBar?.setBackgroundDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.gradient
+            )
+        )
+
+        val textView = findViewById<TextView>(R.id.barcode)
+        val spannable = SpannableString(textView.text)
+        spannable.setSpan(
+            StyleSpan(Typeface.BOLD),
+            0,
+            textView.text.indexOf(':', 1),
+            0
+        )
+        textView.setText(spannable)
     }
 }
