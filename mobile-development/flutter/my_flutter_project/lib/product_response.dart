6f524b924c5d8744656c0aa2e3f51e2c0bb21297
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2024-03-26 16:55:45 +0100

    Added more product fields

--- mobile-development/flutter/my_flutter_project/lib/product_response.dart
+++ mobile-development/flutter/my_flutter_project/lib/product_response.dart
@@ -4,7 +4,6 @@ part 'product_response.g.dart';
 
 @JsonSerializable(explicitToJson: true)
 class ProductResponse {
-  @JsonKey(name: 'response')
   final Products response;
 
   ProductResponse(this.response);
@@ -22,12 +21,39 @@ class ProductResponse {
 
 @JsonSerializable(explicitToJson: true)
 class Products {
-  @JsonKey(name: 'name')
+  final String barcode;
   final String name;
-  @JsonKey(name: 'altName')
   final String altName;
+  final Pictures pictures;
+  final String quantity;
+  final List<String> brands;
+  final List<String> stores;
+  final List<String> manufacturingCountries;
 
-  Products(this.name, this.altName);
+  // Nutriscore
+  // Novascore
+  // Ecoscore
+  final Ingredients ingredients;
+
+  // traces
+  // allergens
+  final Map<String, String> additives;
+
+  // nutrientLevels
+  // nutritionFacts
+  // ingredientsFromPalmOil
+
+  Products(
+      this.barcode,
+      this.name,
+      this.altName,
+      this.pictures,
+      this.quantity,
+      this.brands,
+      this.stores,
+      this.manufacturingCountries,
+      this.ingredients,
+      this.additives);
 
   factory Products.fromJson(Map<String, dynamic> json) =>
       _$ProductsFromJson(json);
@@ -38,4 +64,31 @@ class Products {
   String toString() {
     return 'Products{name: $name, altName: $altName}';
   }
+}
+
+@JsonSerializable(explicitToJson: true)
+class Pictures {
+  final String product;
+  final String front;
+  final String ingredients;
+  final String nutrition;
+
+  Pictures(this.product, this.front, this.ingredients, this.nutrition);
+
+  factory Pictures.fromJson(Map<String, dynamic> json) =>
+      _$PicturesFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PicturesToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class Ingredients {
+  final List<String> list;
+
+  Ingredients(this.list);
+
+  factory Ingredients.fromJson(Map<String, dynamic> json) =>
+      _$IngredientsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$IngredientsToJson(this);
 }
--- mobile-development/flutter/my_flutter_project/lib/product_response.g.dart
+++ mobile-development/flutter/my_flutter_project/lib/product_response.g.dart
@@ -17,11 +17,52 @@ Map<String, dynamic> _$ProductResponseToJson(ProductResponse instance) =>
     };
 
 Products _$ProductsFromJson(Map<String, dynamic> json) => Products(
+      json['barcode'] as String,
       json['name'] as String,
       json['altName'] as String,
+      Pictures.fromJson(json['pictures'] as Map<String, dynamic>),
+      json['quantity'] as String,
+      (json['brands'] as List<dynamic>).map((e) => e as String).toList(),
+      (json['stores'] as List<dynamic>).map((e) => e as String).toList(),
+      (json['manufacturingCountries'] as List<dynamic>)
+          .map((e) => e as String)
+          .toList(),
+      Ingredients.fromJson(json['ingredients'] as Map<String, dynamic>),
+      Map<String, String>.from(json['additives'] as Map),
     );
 
 Map<String, dynamic> _$ProductsToJson(Products instance) => <String, dynamic>{
+      'barcode': instance.barcode,
       'name': instance.name,
       'altName': instance.altName,
+      'pictures': instance.pictures.toJson(),
+      'quantity': instance.quantity,
+      'brands': instance.brands,
+      'stores': instance.stores,
+      'manufacturingCountries': instance.manufacturingCountries,
+      'ingredients': instance.ingredients.toJson(),
+      'additives': instance.additives,
+    };
+
+Pictures _$PicturesFromJson(Map<String, dynamic> json) => Pictures(
+      json['product'] as String,
+      json['front'] as String,
+      json['ingredients'] as String,
+      json['nutrition'] as String,
+    );
+
+Map<String, dynamic> _$PicturesToJson(Pictures instance) => <String, dynamic>{
+      'product': instance.product,
+      'front': instance.front,
+      'ingredients': instance.ingredients,
+      'nutrition': instance.nutrition,
+    };
+
+Ingredients _$IngredientsFromJson(Map<String, dynamic> json) => Ingredients(
+      (json['list'] as List<dynamic>).map((e) => e as String).toList(),
+    );
+
+Map<String, dynamic> _$IngredientsToJson(Ingredients instance) =>
+    <String, dynamic>{
+      'list': instance.list,
     };
